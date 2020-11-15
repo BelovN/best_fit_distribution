b5ced42ed07d2f67cc6bd6a409800eccba714b53
BelovN <nikita.belov.2012@mail.ru>
2020-11-15 17:38:11 +0500

    Парсинг данных

---
+++ main.py
@@ -0,0 +1,149 @@
+import os
+import warnings
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+import statsmodels.api as sm
+import matplotlib
+import matplotlib.pyplot as plt
+
+from settings import DATA_DIR
+
+
+
+matplotlib.rcParams['figure.figsize'] = (16.0, 12.0)
+matplotlib.style.use('ggplot')
+
+# Create models from dataa
+def best_fit_distribution(data, bins=200, ax=None):
+    """Model data by finding best fit distribution to data"""
+    # Get histogram of original data
+    y, x = np.histogram(data, bins=bins, density=True)
+    x = (x + np.roll(x, -1))[:-1] / 2.0
+
+    # Distributions to check
+
+    # Best holders
+    best_distribution = st.norm
+    best_params = (0.0, 1.0)
+    best_sse = np.inf
+    num = 1
+    # Estimate distribution parameters from data
+    for distribution in DISTRIBUTIONS:
+        progress = num / len(DISTRIBUTIONS)
+        print('progress : ' + str(progress * 100) + "%")
+        num += 1
+        # Try to fit the distribution
+        try:
+            # Ignore warnings from data that can't be fit
+            with warnings.catch_warnings():
+                warnings.filterwarnings('ignore')
+
+                # fit dist to data
+                params = distribution.fit(data)
+
+                # Separate parts of parameters
+                arg = params[:-2]
+                loc = params[-2]
+                scale = params[-1]
+
+                # Calculate fitted PDF and error with fit in distribution
+                pdf = distribution.pdf(x, loc=loc, scale=scale, *arg)
+                sse = np.sum(np.power(y - pdf, 2.0))
+
+                # if axis pass in add to plot
+                try:
+                    if ax:
+                        pd.Series(pdf, x).plot(ax=ax)
+                    end
+                except Exception:
+                    pass
+
+                # identify if this distribution is better
+                if best_sse > sse > 0:
+                    best_distribution = distribution
+                    best_params = params
+                    best_sse = sse
+
+        except Exception:
+            pass
+
+    return (best_distribution.name, best_params)
+
+
+def make_pdf(dist, params, size=10000):
+    """Generate distributions's Probability Distribution Function """
+
+    # Separate parts of parameters
+    arg = params[:-2]
+    loc = params[-2]
+    scale = params[-1]
+
+    # Get sane start and end points of distribution
+    start = dist.ppf(0.01, *arg, loc=loc, scale=scale) if arg else dist.ppf(0.01, loc=loc, scale=scale)
+    end = dist.ppf(0.99, *arg, loc=loc, scale=scale) if arg else dist.ppf(0.99, loc=loc, scale=scale)
+
+    # Build PDF and turn into pandas Series
+    x = np.linspace(start, end, size)
+    y = dist.pdf(x, loc=loc, scale=scale, *arg)
+    pdf = pd.Series(y, x)
+
+    return pdf
+
+
+def load_data():
+    file_names = os.listdir(DATA_DIR)
+    data = dict()
+    for file_name in file_names:
+        data[file_name.replace('.csv', '')] = pd.read_csv(os.path.join(DATA_DIR, file_name),
+                                                          sep=',', encoding='utf-8',
+                                                          parse_dates=['Date_UTC'], dayfirst=True,
+                                                          index_col='Date_UTC')
+    return data
+
+
+
+def main():
+    # Load data from statsmodels datasets
+    data = pd.Series(sm.datasets.elnino.load_pandas().data.set_index('YEAR').values.ravel())
+
+    # Plot for comparison
+    plt.figure(figsize=(12,8))
+    ax = data.plot(kind='hist', density=True, bins=50, alpha=0.5, color=plt.rcParams['axes.prop_cycle'].by_key()['color'][1])
+    # Save plot limits
+    dataYLim = ax.get_ylim()
+
+    # Find best fit distribution
+    best_fit_name, best_fit_params = best_fit_distribution(data, 200, ax)
+    best_dist = getattr(st, best_fit_name)
+
+    # Update plots
+    ax.set_ylim(dataYLim)
+    ax.set_title(u'El Niño sea temp.\n All Fitted Distributions')
+    ax.set_xlabel(u'Temp (°C)')
+    ax.set_ylabel('Frequency')
+
+    # Make PDF with best params
+    pdf = make_pdf(best_dist, best_fit_params)
+
+    # Display
+    plt.figure(figsize=(12,8))
+    ax = pdf.plot(lw=2, label='PDF', legend=True)
+    data.plot(kind='hist', bins=50, density=True, alpha=0.5, label='Data', legend=True, ax=ax)
+
+    param_names = (best_dist.shapes + ', loc, scale').split(', ') if best_dist.shapes else ['loc', 'scale']
+    param_str = ', '.join(['{}={:0.2f}'.format(k,v) for k,v in zip(param_names, best_fit_params)])
+    dist_str = '{}({})'.format(best_fit_name, param_str)
+
+    ax.set_title(u'Морская температура Эль-Ниньо. с наилучшим распределением \n' + dist_str)
+    ax.set_xlabel(u'Tемпература. (°C)')
+    ax.set_ylabel('Частота')
+
+    plt.savefig("1.png")
+
+
+data = load_data()
+
+#
+# if __name__ == '__main__':
+#     main()
